61251d240f8a2ecf8bebc30d729ac5761e386a6f
pancake <pancake@nowsecure.com>
2022-06-16 12:51:33 +0200

    Remove all the cmake references

--- src/cmake/Utils.cmake
+++
@@ -1,6 +0,0 @@
-
-# Like option(), but the value can also be AUTO
-macro(tri_option name desc default)
-    set("${name}" "${default}" CACHE STRING "${desc}")
-    set_property(CACHE "${name}" PROPERTY STRINGS AUTO ON OFF)
-endmacro()
