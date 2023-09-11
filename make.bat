d16882c20c32d6a26d8f3807cce937187062d9f0
FetheredSerpent <144615238+FetheredSerpent@users.noreply.github.com>
2023-09-11 11:00:25 +0530

    Restore functionality
    Restored some bundling functionality that was accidently disabled

--- make.bat
+++ make.bat
@@ -1,27 +1,24 @@
-SET "R2DIST=r2_dist"
+SET "R2DIR=radare2"
 SET "BUILDDIR=build_%PLATFORM%"
 
 ECHO Preparing directory
-rem RMDIR /S /Q %BUILDDIR%
-rem MKDIR %BUILDDIR%
-rem CD %BUILDDIR%
-rem nmake
-rem meson configure --buildtype=release ..\src
-rem CD ..\release
-rem ninja -v -j4
-CD src && meson --buildtype=release ..\build
-CD ..\build && ninja -j4
+IF EXIST %BUILDDIR% (
+RMDIR /S /Q %BUILDDIR%
+)
+
+CD src && meson --buildtype=release ..\%BUILDDIR%
+CD ..\%BUILDDIR% && ninja -j4
 REM IF !ERRORLEVEL! NEQ 0 EXIT /B 1
 CD ..
 
 ECHO Deploying iaito
 MKDIR iaito
-COPY build\iaito.exe iaito\iaito.exe
-RMDIR /S /Q build
-rem XCOPY /S /I ..\%R2DIST%\share iaito\share
-rem XCOPY /S /I ..\%R2DIST%\lib iaito\lib
-rem DEL iaito\lib\*.lib
-rem COPY ..\%R2DIST%\bin\*.dll iaito\
+COPY %BUILDDIR%\iaito.exe iaito\iaito.exe
+RMDIR /S /Q %BUILDDIR%
+XCOPY /S /I %R2DIR%\share iaito\share
+XCOPY /S /I %R2DIR%\lib iaito\lib
+DEL iaito\lib\*.lib
+COPY %R2DIR%\bin\*.dll iaito\
 windeployqt iaito\iaito.exe
 FOR %%i in (..\src\translations\*.qm) DO MOVE "%%~fi" iaito\translations
 
