7ebc3fed7823555d1c9aa074421c761b0fd01381
pancake <pancake@nopcode.org>
2025-06-07 12:15:35 +0200

    Add subst script from radare2/sys

---
+++ scripts/subst.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+a="$1"
+b="$2"
+if [ -z "$a" ]; then
+	echo "Usage: sys/subst.sh [old] [new]"
+	exit 1
+fi
+#[ -z "$a" ] && a="r_str_chop "
+#[ -z "$b" ] && b="r_str_trim "
+WHERE="src"
+git grep "$a" ${WHERE} | less -R
+if [ -n "$b" ]; then
+	FILES=`git grep "$a" ${WHERE} | cut -d : -f 1 | uniq `
+	for f in $FILES ; do
+		echo "Sedded $a"
+		sed -e "s/$a/$b/g" < $f > $f.sub
+		mv $f.sub $f
+	done
+else
+	echo "Oops"
+fi
