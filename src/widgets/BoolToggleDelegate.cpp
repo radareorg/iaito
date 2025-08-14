020cf9384655a5706c1ca61da0d158408d4bae4c
ApkUnpacker <apkunpacker@gmail.com>
2025-08-14 22:00:36 +0530

    Update BoolToggleDelegate.cpp

--- src/widgets/BoolToggleDelegate.cpp
+++ src/widgets/BoolToggleDelegate.cpp
@@ -1,5 +1,17 @@
 #include "BoolToggleDelegate.h"
 #include <QEvent>
+#include <QVariant>
+#include <QMetaType>
+
+namespace {
+static inline int variantTypeId(const QVariant &v) {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+    return v.metaType().id();
+#else
+    return v.userType();
+#endif
+}
+} // namespace
 
 BoolTogggleDelegate::BoolTogggleDelegate(QObject *parent)
     : QStyledItemDelegate(parent)
@@ -8,7 +20,7 @@ BoolTogggleDelegate::BoolTogggleDelegate(QObject *parent)
 QWidget *BoolTogggleDelegate::createEditor(
     QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
 {
-    if (index.data(Qt::EditRole).typeId() == QMetaType::Bool) {
+    if (variantTypeId(index.data(Qt::EditRole)) == QMetaType::Bool) {
         return nullptr;
     }
     return QStyledItemDelegate::createEditor(parent, option, index);
@@ -23,7 +35,7 @@ bool BoolTogggleDelegate::editorEvent(
     if (model->flags(index).testFlag(Qt::ItemFlag::ItemIsEditable)) {
         if (event->type() == QEvent::MouseButtonDblClick) {
             auto data = index.data(Qt::EditRole);
-            if (data.typeId() == QMetaType::Bool) {
+            if (variantTypeId(data) == QMetaType::Bool) {
                 model->setData(index, !data.toBool());
                 return true;
             }
